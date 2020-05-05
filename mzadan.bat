f5362046c8572a12ad783dc270cddb99a2066451
Thomas Stricker <strickert0@gmail.com>
2020-05-05 10:39:01 +0200

    Updating software, tutorial and resources (v.1.1)
    Updated versions of the software, tutorial, and resources for mzAdan v1.1

---
+++ mzadan.bat
@@ -0,0 +1,13 @@
+{\rtf1\ansi\ansicpg1252\cocoartf1504
+{\fonttbl\f0\fnil\fcharset0 Menlo-Regular;}
+{\colortbl;\red255\green255\blue255;\red36\green38\blue41;\red235\green236\blue237;\red37\green127\blue159;
+\red43\green39\blue19;}
+{\*\expandedcolortbl;\csgray\c100000;\cssrgb\c18824\c20000\c21176;\cssrgb\c93725\c94118\c94510;\cssrgb\c16863\c56863\c68627;
+\cssrgb\c22353\c20000\c9412;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww10800\viewh8400\viewkind0
+\deftab720
+\pard\pardeftab720\sl300\partightenfactor0
+
+\f0\fs26 \cf2 \cb3 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2 java -jar \cf4 \strokec4 mzadan\cf2 \strokec2 .jar\cf5 \strokec5 \
+}
